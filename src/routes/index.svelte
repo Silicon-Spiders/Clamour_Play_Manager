634ef9ee7ed303347c9ddd25da2d46a14f46afd9
Carrasco-R <79488710+Carrasco-R@users.noreply.github.com>
2021-09-22 16:36:06 -0400

    (pseudo) login page

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,33 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div class="loginPortal">
+    <form action="POST">
+        <h1>Login</h1>
+        <div class="field">
+            <label for="user">Username</label>
+            <input name="user" id="user">
+        </div>
+        <div class="field">
+            <label for="pass">Password</label>
+            <input type="password" name="pass" id="pass">
+        </div>
+        <button type="submit">Login</button>
+        
+    </form>
+    <a href="PLACEHOLDER/PUT-NEXT-PAGE-HERE">Temporary Link to another page (So that others can advance)</a>
+</div>
+<style>
+    .loginPortal{
+        background-color:lightgrey;
+        border-radius: 10px;
+        margin: auto;
+        padding: 30px 0px;
+        text-align: center;
+        width: 700px;
+    }
+    .field{
+        display: block;
+    }
+    .field input, .field label{
+        margin: 10px;
+        width: 300px;
+    }
+</style>
