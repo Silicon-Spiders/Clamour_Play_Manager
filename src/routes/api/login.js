8a2f78c3834de56b8dcd7ed4a56dc13400dda04d
Ricardo <therick1016@gmail.com>
2021-09-30 10:58:15 -0400

    test

--- src/routes/api/login.js
+++ src/routes/api/login.js
@@ -1,14 +1,8 @@
 export const post = (req) => {
-    console.log("req.body", req.body);
-
     if(req.body.user == "admin" && req.body.pass == "pass"){
-        return{
-            body: "true"
-        }
+        return{body: "true"}
     }
     else{
-        return{
-            body: "false",
-        }
+        return{body: "false"}
     }
 }
