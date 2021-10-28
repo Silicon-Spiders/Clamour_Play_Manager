8fe04acab85dc3e5ea17a600b806a780fdd78846
Branmn98 <61074729+Branmn98@users.noreply.github.com>
2021-10-28 15:27:50 -0400

    Dummy hook and manage evaluators

---
+++ src/routes/portal/administrator/manage-evaluators.svelte
@@ -0,0 +1,39 @@
+<script>
+    import { } from "os";
+    
+</script>
+
+<svelte:head>
+    <title>Manage Evaluators</title>
+</svelte:head>
+
+<table style="width: 90%;">
+
+    <th align="left">
+        <h4>Evaluators</h4>
+    </th>
+
+    <td>
+        <br>
+        <label for="Last Name">Last Name:</label>
+        <label for="First Name">First Name:</label>
+        <label for="phone">Phone Number:</label>
+        <label for="Email">Email:</label>
+
+        <input type="text" id="Last Name" name="Last Name" readonly>
+
+        <input type="text" id="First Name" name="First Name" readonly>
+
+        <input type="tel" id="phone" name="phone" readonly>
+
+        <input type="email" id="Email" name="Email" readonly>
+
+    </td>
+</table>
+
+<style>
+
+    td {
+        border: 2px;
+    }
+</style>
