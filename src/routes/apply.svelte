e69bc0a33961f1ec5df481453f3eb157e18a7719
WilliamRelken <relkenwilliam@gmail.com>
2021-11-28 16:46:52 -0500

    created a layout component that can be called to specific pages, added tab image and reoworked apply page scaling

--- src/routes/apply.svelte
+++ src/routes/apply.svelte
@@ -43,45 +43,21 @@
     const alert = await formUpload(newForm);
     console.log(alert);
   }
-  let src = "images/clamour-logo-600.jpg";
+  import OutsideLayout from "./outside-layout.svelte";
 </script>
 
 <svelte:head>
   <title>Apply</title>
   <!-- Main Script Submission Page -->
 </svelte:head>
-<header>
-  <a href="/"><img {src} alt="Something" /></a>
-  <nav>
-    <a href="/apply">APPLY</a>
-    <a href="/login">LOGIN</a>
-  </nav>
-</header>
+
+<OutsideLayout/>
 
 <form on:submit|preventDefault={submit}>
-  <table align="center" style="width: 90%;">
-    <colgroup>
-      <col span="1" style="width: 25%;" />
-      <col span="" style="width: 25%;" />
-      <col span="1" style="width: 25%;" />
-      <col span="1" style="width: 25%;" />
-    </colgroup>
-
-    <th align="center">
-      <h4>Play Info</h4>
-    </th>
-    <th align="center">
-      <h4>Personal Details</h4>
-    </th>
-    <th align="center">
-      <h4>Contact Information</h4>
-    </th>
-    <th align="center">
-      <h4>Finish & Submit</h4>
-    </th>
-    <tr>
-      <td class="card">
-        <label for="play_pdf">Play PDF:</label>
+  <div class="wrap" align="center" style="width: 100%;">
+    <div class="card" span="1">
+      <h3>Play Info</h3>
+      <label for="play_pdf">Play PDF:</label>
         <input
           type="file"
           accept="application/pdf"
@@ -105,16 +81,10 @@
           rows="10"
           placeholder="Please explain why this many actors can perform this play."
         />
-        <br>
-
-        <label for="tone">Tone of the Play: </label>
-        <select name="tone" id="tone">
-          <option value="drama">Drama</option>
-          <option value="comedy">Comedy</option>
-        </select>
-      </td>
-      <td class="card">
-        <label for="prof_intro">Professional Introduction:</label>
+    </div>
+    <div class="card" span="1">
+      <h3>Personal Details</h3>
+      <label for="prof_intro">Professional Introduction:</label>
         <br />
         <textarea
           name="prof_intro"
@@ -133,9 +103,10 @@
           rows="7"
           placeholder="Tell us about yourself."
         />
-      </td>
-      <td class="card">
-        <label><span>First Name:</span><br><input type="text" name="fname"></label><br>
+    </div>
+    <div class="card" span="1">
+      <h3>Contact Information</h3>
+      <label><span>First Name:</span><br><input type="text" name="fname"></label><br>
         <label><span>Last Name:</span><br><input type="text" name="lname"></label><br>
         <label><span>Country:</span><br><input type="text" name="counrty"></label><br>
         <label><span>Street Address:</span><br><input type="text" name="address"></label><br>
@@ -149,9 +120,10 @@
           <option value="online">Online</option>
         </select>
         </label>
-      </td>
-      <td class="card">
-        <label for="synopsis">Synopsis:</label>
+    </div>
+    <div class="card" span="1">
+      <h3>Finish & Submit</h3>
+      <label for="synopsis">Synopsis:</label>
         <br />
         <textarea
           name="synopsis"
@@ -172,21 +144,28 @@
         />
         <br />
         <button type="submit">Submit</button>
-      </td>
-    </tr>
-  </table>
+    </div>
+  </div>
 </form>
 
 <style>
   .card {
     border-radius: 25px;
     background: #ffadad;
-    padding: 20px;
-    width: 25%;
-    height: 150px;
+    padding: 10px;
+    margin: 5px;
+    display: inline-table;
+    width: 300px;
+    min-height: 420px;
+  }
+  .card textarea {
+    resize: vertical;
   }
   .card input{
     margin-left: 20pt;
     width: 80%;
   }
+  .wrap {
+    padding: 20px;
+}
 </style>
