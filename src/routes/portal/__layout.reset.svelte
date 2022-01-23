47d36f5e3b1965af084dfe79a9e5defd90c7060d
WilliamRelken <relkenwilliam@gmail.com>
2022-01-23 17:49:15 -0500

    added separate links for admin/eval

--- src/routes/portal/__layout.reset.svelte
+++ src/routes/portal/__layout.reset.svelte
@@ -1,32 +1,45 @@
 <script>
-import { goto } from "$app/navigation";
+  import { goto } from "$app/navigation";
 
   import "$lib/portal-styles.scss";
+
+  let userRole = "admin";
+
 </script>
+
+
 
 <div class="nav-bar">
   <div class="nav-links">
-    <a href="./view-plays"><span class="material-icons">source</span>View</a>
-    <a href="./assign-plays"
-      ><span class="material-icons">assignment</span>Assign</a
-    >
-    <a href="./account-creation"
-      ><span class="material-icons">manage_accounts</span>Manage Evaluators</a
-    >
-    <a id="logout" href="/">
-      <span class="material-icons"> logout </span>Logout</a
-    >
+    {#if userRole == "admin"}
+      <a href="/portal/administrator/view-plays"><span class="material-icons">source</span>View</a>
+      <a href="/portal/administrator/assign-plays"><span class="material-icons">assignment</span>Assign</a>
+      <a href="/portal/administrator/account-creation"><span class="material-icons">manage_accounts</span>Manage Evaluators</a>
+    {:else if userRole == "evaluator"}
+      <a href="/portal/evaluator/main">Plays</a>
+      <!-- <a href="../evaluator/assign-plays">Assign</a> -->
+    {/if}
+    <a id="logout" href="/"><span class="material-icons"> logout </span>Logout</a>
   </div>
 </div>
 <body>
   <div class="redback">
     <div class="main">
-      <div class="account" on:click={() => goto("/portal/administrator/admin-profile")}>
-        <span class="account-name">
-          Elaine Smith
-          <span class="material-icons">account_circle</span>
-        </span>
-      </div>
+      {#if userRole == "admin"}
+        <div class="account" on:click={() => goto("/portal/administrator/admin-profile")}>
+          <span class="account-name">
+            Elaine Smith
+            <span class="material-icons">account_circle</span>
+          </span>
+        </div>
+      {:else if userRole == "evaluator"}
+        <div class="account" on:click={() => goto("../evaluator/main")}>
+          <span class="account-name">
+            Evaluator
+            <span class="material-icons">account_circle</span>
+          </span>
+        </div>
+      {/if}
       <div class="content">
         <slot />
       </div>
