bb374f554bcfd36260e74415d6c521ecdc15ac32
will <will@spinspire.com>
2022-02-23 20:00:04 -0500

    remove svelte.kit.target for sveltekit update

--- svelte.config.js
+++ svelte.config.js
@@ -6,8 +6,6 @@ const config = {
   preprocess: preprocess(),
 
   kit: {
-    // hydrate the <div id="svelte"> element in src/app.html
-    target: "#svelte",
     adapter: node({ env: { port: process.env.PORT } }),
   },
 };
